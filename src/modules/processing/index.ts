34633893dba2636b2a5177955b5704456f040bcc
yxlsalah1 <mango4356w@gmail.com>
2026-06-10 18:53:26 +0200

    Add processing module index

---
+++ src/modules/processing/index.ts
@@ -0,0 +1,6 @@
+/**
+ * Effect Pipeline Orchestrator
+ */
+export { FrameProcessor } from './FrameProcessor';
+export { FrameBufferManager } from './FrameBuffer';
+export { GPURender } from './GPURender';
